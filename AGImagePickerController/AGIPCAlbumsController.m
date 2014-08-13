f9606182f2288ed500db627e7ddf7645d2346128
Alexander Belyavskiy <diejmon@gmail.com>
2014-08-13 16:39:35 +0300

    remove x64 warnings

--- AGImagePickerController/AGIPCAlbumsController.m
+++ AGImagePickerController/AGIPCAlbumsController.m
@@ -144,7 +144,7 @@
     NSUInteger numberOfAssets = group.numberOfAssets;
     
     cell.textLabel.text = [NSString stringWithFormat:@"%@", [group valueForProperty:ALAssetsGroupPropertyName]];
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", numberOfAssets];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)numberOfAssets];
     [cell.imageView setImage:[UIImage imageWithCGImage:[(ALAssetsGroup *)self.assetsGroups[indexPath.row] posterImage]]];
 	[cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
 	
